6e2237e0a5c807e84daf7632a2f0aa5cbb7374c0
BRIANCHERUIYOT <brian.cheruiyot@student.moringaschool.com>
2021-09-19 22:30:51 +0300

    adds the id for total price tag

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ const Pizza=function(size,crust,toppings) {
 
 $(document).ready(function() {
 
-    $("#Add").click(function() {
+    $("#Add").click(function(event) {
+      event.preventDefault();
     let Ppizza = parseInt($("#size option:selected").val());
     let Ptopping = parseInt($("#top option:selected").val());
     let Pcrust = parseInt($("#crust option:selected").val()); 
@@ -31,6 +32,17 @@ $(document).ready(function() {
       var Ttopping= $("#topping option:selected").text();
       var Tname= $("#name option:selected").text();
       
+      $("tbody:last").append("<tr>"+"<td>"+Tname+"</td>"+"<td>"+Tpizza+"</td>"+"<td>" +Tcrust+ "</td>" +"<td>"+Ttopping+ "</td>" + "<td>" + price +"</td>"+"</tr>")
 
     })
+
+    $("button#final").click(function(event){
+      $("#price:last").text(price);
+      $("#last").show();
+      
+      
+          alert("thanks  for your add and you could make some more if you want, this will be delivered in on time")
+    
+
+  })
 });
